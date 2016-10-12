ee158aab722dd46f047061ab9ef114dced80348a
Yuanqin Deng <yuanqindeng@DYQMB.local>
2016-10-12 22:40:01 +0800

    commit for login3

---
+++ assets/js/scripts.js
@@ -0,0 +1,21 @@
+
+function login() {
+	console.log("......");
+	var username = document.getElementById('form-username').value;
+	var password = document.getElementById('form-password').value;
+	if(username.trim()==null||username.trim()==''||password.trim()==null||password.trim()==''){
+		return;
+	}
+	if(validateUser(username,password)){
+		window.location.href = "./success.html";
+	}else{
+		window.location.href = "./error.html";
+	}
+}
+function validateUser(name, passd) {
+	if(name=='admin'&&passd=='admin'){
+		return true;
+	}else{
+		return false;
+	}
+}
